9f42c3be42491581e2e2867928c0d707a938b803
gxf1212 <gxf1212@zju.edu.cn>
2024-03-17 09:15:28 -0400

    weatherReport

--- javascript-programming-essentials/weatherReport/weather_report.js
+++ javascript-programming-essentials/weatherReport/weather_report.js
@@ -1,3 +1,22 @@
 function showweatherDetails(event) {
     event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '6c7a56e9bbcd79ca1f2211708a969c5a'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+          });
 }
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
